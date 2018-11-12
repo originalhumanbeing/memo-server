74ae9d46165ca4f31c695545de0631763d72143d
originalhumanbeing <han.hyunjung17@gmail.com>
2018-11-12 17:12:29 +0900

    Refactor client side by separating notepad into classes

--- client/notepad.js
+++ client/notepad.js
@@ -1,10 +1,138 @@
-class Notepad {
+let currentUser = '';
+let currentFile = '';
+let cursorStart = '';
+let cursorEnd = '';
+
+class List {
     constructor() {
+        this.list = document.querySelector('.list');
+        this.showList();
+    }
+
+    showList() {
+        fetch(`http://localhost:8080/memos/${currentUser}`, {
+            method: 'get'
+        }).then((res) => res.json()).then((data) => {
+            if (!data['body'] || data['body'].length === 0) return;
+
+            this.list.innerHTML = '';
+            data['body'].sort((a, b) => a - b);
+            for (let memo of data['body']) {
+                let li = document.createElement('li');
+                li.classList.add(memo);
+                li.innerText = memo;
+                li.addEventListener('click', (e) => {
+                    new TextArea(e);
+                });
+                this.list.appendChild(li);
+            }
+        })
+    }
+}
+
+class TextArea {
+    constructor(e) {
+        this.textarea = document.querySelector('.memo');
+        this.findCursor();
+        this.showMemo(e);
+    }
+
+    findCursor() {
+        this.textarea.addEventListener('keypress', () => {
+            cursorStart = this.textarea.selectionStart;
+            cursorEnd = this.textarea.selectionEnd;
+        })
+    }
+
+    showMemo(e) {
+        currentFile = e.target.className;
+        fetch(`http://localhost:8080/memo/${currentUser}/${currentFile}`, {
+            method: 'get'
+        }).then((res) => res.json()).then((data) => {
+            this.textarea.value = data.body.content;
+            this.textarea.setSelectionRange(data.body.cursorStart, data.body.cursorEnd);
+            this.textarea.focus();
+        })
+    }
+}
+
+class Menu {
+    constructor() {
+        this.textarea = document.querySelector('.memo');
         this.writeBtn = document.querySelector('.writeBtn');
         this.saveBtn = document.querySelector('.saveBtn');
         this.updateBtn = document.querySelector('.updateBtn');
         this.deleteBtn = document.querySelector('.deleteBtn');
 
+        this.writeBtn.addEventListener('click', () => {
+            this.textarea.value = '';
+        });
+
+        this.saveBtn.addEventListener('click', () => {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            cursorStart = this.textarea.selectionStart;
+            cursorEnd = this.textarea.selectionEnd;
+
+            fetch(`http://localhost:8080/memo/${currentUser}`, {
+                method: 'post',
+                headers: myHeaders,
+                body: JSON.stringify({
+                    memo: this.textarea.value,
+                    user: currentUser,
+                    cursorStart: cursorStart,
+                    cursorEnd: cursorEnd
+                })
+            }).then((res) => res.json()).then((data) => {
+                this.textarea.value = data.body.content;
+                cursorStart = data.body.cursorStart;
+                cursorEnd = data.body.cursorEnd;
+                currentFile = data.body.title;
+                new List();
+            })
+        });
+
+        this.updateBtn.addEventListener('click', () => {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            cursorStart = this.textarea.selectionStart;
+            cursorEnd = this.textarea.selectionEnd;
+
+            fetch(`http://localhost:8080/memo/${currentUser}/${currentFile}`, {
+                method: 'put',
+                headers: myHeaders,
+                body: JSON.stringify({
+                    memo: this.textarea.value,
+                    user: currentUser,
+                    cursorStart: cursorStart,
+                    cursorEnd: cursorEnd
+                })
+            }).then((res) => res.json()).then((data) => {
+                this.textarea.value = data.body.content;
+                cursorStart = data.body.cursorStart;
+                cursorEnd = data.body.cursorEnd;
+                currentFile = data.body.title;
+                new List();
+            })
+        });
+
+        this.deleteBtn.addEventListener('click', () => {
+            fetch(`http://localhost:8080/memo/${currentUser}/${currentFile}`, {
+                method: 'delete'
+            }).then((res) => res.json()).then(data => {
+                window.alert(data.body);
+            }).then(() => {
+                new List();
+            });
+            this.textarea.value = '';
+        });
+    }
+}
+
+class Notepad {
+    constructor() {
         this.textarea = document.querySelector('.memo');
         this.list = document.querySelector('.list');
 
@@ -18,14 +146,19 @@ class Notepad {
         this.userNickname = document.querySelector('#userNickname');
         this.logoutBtn = document.querySelector('#logoutBtn');
 
-        this.currentUser = '';
-        this.currentFile = '';
-        this.cursorStart = '';
-        this.cursorEnd = '';
+        this.logIn();
+        this.logOut();
 
-        this.addClickEvents();
-        this.findCursor();
-        this.showMemo();
+        new Menu();
+    }
+
+    logIn() {
+        this.loginBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            let id = this.id.value;
+            let pwd = this.pwd.value;
+            this.validateUser(id, pwd);
+        });
     }
 
     validateUser(id, pwd) {
@@ -44,16 +177,16 @@ class Notepad {
             }
 
             if (data['lastMemoContent']) {
-                this.currentFile = data['lastMemoContent'];
+                currentFile = data['lastMemoContent'];
                 this.textarea.setSelectionRange(data['lastMemoContent'].cursorStart, data['lastMemoContent'].cursorEnd);
                 this.textarea.focus();
             }
             this.textarea.value = '';
-            this.currentFile = '';
-            this.currentUser = data['body'].nickname;
-            this.showList();
+            currentFile = '';
+            currentUser = data['body'].nickname;
+            new List();
             this.authForm.hidden = true;
-            this.userNickname.innerText = this.currentUser + '님';
+            this.userNickname.innerText = currentUser + '님';
             this.userNav.hidden = false;
             this.authFailMsg.hidden = true;
         }).then(() => {
@@ -62,124 +195,13 @@ class Notepad {
         })
     }
 
-    findCursor() {
-        this.textarea.addEventListener('keypress', () => {
-            this.cursorStart = this.textarea.selectionStart;
-            this.cursorEnd = this.textarea.selectionEnd;
-        })
-    }
-
-    showList() {
-        fetch(`http://localhost:8080/memos/${this.currentUser}`, {
-            method: 'get'
-        }).then((res) => res.json()).then((data) => {
-            if (!data['body'] || data['body'].length === 0) return;
-
-            this.list.innerHTML = '';
-            data['body'].sort((a, b) => a - b);
-            for (let memo of data['body']) {
-                let li = document.createElement('li');
-                li.classList.add(memo);
-                li.innerText = memo;
-                li.addEventListener('click', this.showMemo(this));
-                this.list.appendChild(li);
-            }
-        })
-    }
-
-    showMemo(notepad) {
-        return e => {
-            notepad.currentFile = e.target.className;
-            fetch(`http://localhost:8080/memo/${notepad.currentUser}/${notepad.currentFile}`, {
-                method: 'get'
-            }).then((res) => res.json()).then((data) => {
-                notepad.textarea.value = data.body.content;
-                notepad.textarea.setSelectionRange(data.body.cursorStart, data.body.cursorEnd);
-                notepad.textarea.focus();
-            })
-        }
-    }
-
-    addClickEvents() {
-        this.loginBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            let id = this.id.value;
-            let pwd = this.pwd.value;
-            this.validateUser(id, pwd);
-        });
-
+    logOut() {
         this.logoutBtn.addEventListener('click', () => {
-            this.currentUser = '';
-            this.currentFile = '';
+            currentUser = '';
+            currentFile = '';
             this.list.innerHTML = '';
             this.authForm.hidden = false;
             this.userNav.hidden = true;
-        });
-
-        this.writeBtn.addEventListener('click', () => {
-            this.textarea.value = '';
-        });
-
-        this.saveBtn.addEventListener('click', () => {
-            const myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-
-            this.cursorStart = this.textarea.selectionStart;
-            this.cursorEnd = this.textarea.selectionEnd;
-
-            fetch(`http://localhost:8080/memo/${this.currentUser}`, {
-                method: 'post',
-                headers: myHeaders,
-                body: JSON.stringify({
-                    memo: this.textarea.value,
-                    user: this.currentUser,
-                    cursorStart: this.cursorStart,
-                    cursorEnd: this.cursorEnd
-                })
-            }).then((res) => res.json()).then((data) => {
-                this.textarea.value = data.body.content;
-                this.cursorStart = data.body.cursorStart;
-                this.cursorEnd = data.body.cursorEnd;
-                this.currentFile = data.body.title;
-                this.showList();
-            })
-        });
-
-        this.updateBtn.addEventListener('click', () => {
-            const myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-
-            this.cursorStart = this.textarea.selectionStart;
-            this.cursorEnd = this.textarea.selectionEnd;
-
-            fetch(`http://localhost:8080/memo/${this.currentUser}/${this.currentFile}`, {
-                method: 'put',
-                headers: myHeaders,
-                body: JSON.stringify({
-                    memo: this.textarea.value,
-                    user: this.currentUser,
-                    cursorStart: this.cursorStart,
-                    cursorEnd: this.cursorEnd
-                })
-            }).then((res) => res.json()).then((data) => {
-                console.log('update된 데이터', data);
-                this.textarea.value = data.body.content;
-                this.cursorStart = data.body.cursorStart;
-                this.cursorEnd = data.body.cursorEnd;
-                this.currentFile = data.body.title;
-                this.showList();
-            })
-        });
-
-        this.deleteBtn.addEventListener('click', () => {
-            fetch(`http://localhost:8080/memo/${this.currentUser}/${this.currentFile}`, {
-                method: 'delete'
-            }).then((res) => res.json()).then(data => {
-                window.alert(data.body);
-            }).then(res => {
-                this.showList();
-            });
-            this.textarea.value = '';
         });
     }
 }
