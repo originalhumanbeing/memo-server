85af620840cb85aab9a4153a530e3165b916ccc5
originalhumanbeing <han.hyunjung17@gmail.com>
2018-12-20 19:03:36 +0900

    Refactor login

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const models = require('../models'),
 router.post('/signup', asyncErrorHandle(async (req, res) => {
     const {nickname, email, pwd, checkpwd} = req.body;
 
-    assertItemsExist(nickname, email, pwd, checkpwd);
+    assertSignupItemsExist(nickname, email, pwd, checkpwd);
     await assertEmailIsNotDuplicated(email);
     // TODO: email 형식 validation 하기
     // assertEmailFormIsValid(email);
@@ -19,47 +19,28 @@ router.post('/signup', asyncErrorHandle(async (req, res) => {
     await signup(pwd, nickname, email);
 
     res.status(201);
-    res.json({success: 'ok', body: '이제 Memo Memo를 사용해보세요!'});
+    res.json({success: 'ok', msg: '이제 Memo Memo를 사용해보세요!'});
 }));
 
 // login 하기
 router.post('/login', asyncErrorHandle(async (req, res) => {
     const {id, pwd} = req.body;
-
-    if (!id || !pwd) {
-        res.status(200);
-        res.json({msg: '로그인이 필요합니다!'});
-        return;
-    }
-
     let thisMember = await models.Member.findOne({where: {email: id}});
-    if (thisMember === null) {
-        res.status(200);
-        res.json({msg: '아이디가 존재하지 않습니다!'});
-        return;
-    }
 
-    const encryptedPwd = await auth.pbkdf2Async(pwd, auth.SALT);
-    if (encryptedPwd !== thisMember.dataValues.pwd) {
-        res.status(200);
-        res.json({msg: '비밀번호가 일치하지 않습니다!'});
-        return;
-    }
+    assertLoginItemsExist(id, pwd);
+    await assertIdExist(id, thisMember);
+    await assertPasswordIsCorrect(pwd, thisMember);
 
     const nickname = thisMember.dataValues.nickname;
     const token = await auth.createToken(nickname);
 
     res.status(200);
-    res.json({
-        token,
-        nickname,
-        msg: "로그인이 성공했습니다!"
-    });
+    res.json({token, nickname});
 }));
 
-function assertItemsExist(nickname, email, pwd, checkpwd) {
+function assertSignupItemsExist(nickname, email, pwd, checkpwd) {
     if (!nickname || !email || !pwd || !checkpwd) {
-        throw new UserException(400, '모든 항목을 작성해주셔야 가입이 가능합니다');
+        throw new UserException(400, '모든 항목을 입력해주셔야 가입이 가능합니다');
     }
 }
 
@@ -78,7 +59,6 @@ async function assertNicknameIsNotDuplicated(nickname) {
 }
 
 function assertPasswordIsMatched(pwd, checkpwd) {
-
     if (pwd !== checkpwd) {
         throw new UserException(400, '비밀번호가 일치하지 않습니다');
     }
@@ -90,6 +70,19 @@ async function signup(pwd, nickname, email) {
     if (newMember == null) {
         throw new UnexpectedException('회원가입을 다시 시도해주세요');
     }
+}
+
+function assertLoginItemsExist(id, pwd) {
+    if (!id || !pwd) throw new UserException(400, '모든 항목을 입력해주셔야 로그인이 가능합니다');
+}
+
+async function assertIdExist(id, thisMember) {
+    if (thisMember === null) throw new UserException(400, '아이디가 존재하지 않습니다');
+}
+
+async function assertPasswordIsCorrect(pwd, thisMember) {
+    const encryptedPwd = await auth.pbkdf2Async(pwd, auth.SALT);
+    if (encryptedPwd !== thisMember.dataValues.pwd) throw new UserException(400, '비밀번호가 일치하지 않습니다');
 }
 
 module.exports = router;
